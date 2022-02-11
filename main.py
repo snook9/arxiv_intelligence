021e607f87ca805c110405d3d4f7982dfd4b989c
snook9 <jonathan.cassaing@free.fr>
2022-02-11 23:56:57 +0100

    Adding logging system

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Highlighting the relationship between authors and scientists
 """
 
 import time
+import logging
+from pathlib import Path
 from datetime import datetime
 from services.api.arxiv_api import ArxivApi
 from services.api.ner_api import NerApi
@@ -18,9 +20,19 @@ if __name__ == '__main__':
 
     print("arXiv Intelligence v0.0.1")
 
+    # We create the log folder
+    log_folder = Path("log")
+    if False is log_folder.exists():
+        # If the folder doesn't exist, we create it
+        log_folder.mkdir()
+    # Creating log config
+    today = datetime.today().strftime("%Y-%m-%d-%H-%M-%S.%f")
+    logging.basicConfig(filename=log_folder.joinpath(today + ".log"), encoding='utf-8', level=logging.DEBUG)
+
     # We retreive the PDF documents
     pdf_list = ArxivApi(MAX_PDF_NUMBER).get_pdf()
     print(len(pdf_list), "PDF file(s) retrieved")
+    logging.info("%s PDF file(s) retrieved", len(pdf_list))
 
     # We instantiate an ontology
     ontology_service = OntologyService()
@@ -31,10 +43,10 @@ if __name__ == '__main__':
         ner_api = NerApi(BASE_WS_URL)
         message = ner_api.post_document(pdf)
         if message is None:
-            print("Error while sending the file", pdf)
+            logging.error("Error while sending the file: %s", pdf)
             continue
 
-        print("ID:", message.object_id, "|", message.message)
+        logging.info("ID: %s | %s", message.object_id, message.message)
 
         # If the PDF has been sent
         if message.object_id != -1:
@@ -46,18 +58,18 @@ if __name__ == '__main__':
                 document = ner_api.get_document_metadata(message.object_id)
                 STATUS = document.status
 
-            print("ID:", message.object_id, "|", "named entities retrieved")
+            logging.info("ID: %s | named entities retrieved", message.object_id)
 
             # Then, we add each named entity of the document
             # to the ontology
             for named_entity in document.named_entities:
                 ontology_service.add_named_entity(named_entity)
 
-            print("ID:", message.object_id, "|", "named entities added to the ontology")
+            logging.info("ID: %s | named entities added to the ontology", message.object_id)
 
     # At the end of the PDF list
     # We write the ontology in a folder
-    today = datetime.today().strftime("%Y-%m-%d-%H-%M-%S.%f")
     filepath = "owl/output_" + today + ".owl"
     ontology_service.save(filepath)
     print("The ontology '" + filepath + "' has been saved!")
+    logging.info("The ontology '%s' has been saved!", filepath)
