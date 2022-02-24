aca0c0cab40511d57d9d8dfc73436c100582dcc7
snook9 <jonathan.cassaing@free.fr>
2022-02-24 23:22:13 +0100

    Escaping illegal characters before ontology save

--- services/ontology/ontology_service.py
+++ services/ontology/ontology_service.py
@@ -23,13 +23,27 @@ class OntologyService():
             self._foaf = self._onto.get_imported_ontologies().first().load()
         except AttributeError as err:
             print(f"Warning! the foaf ontology is not imported in the local ontology: {err}")
+    
+    def _escape_value(self: object, text: str):
+        """Escape the illegal characters for an ontology property"""
+        if text is None:
+            return
+        # function to escape XML character data
+        text = escape(text)
+        text = text.replace('|', '')
+        text = text.replace('\\', '')
+        text = text.replace('{', '')
+        text = text.replace('}', '')
+        text = text.replace('', '')
+        text = text.replace('', '')
+        return text
 
     def _add_authors(self: object, authors, arxiv_document):
         """Add an authors list to the ontology"""
         for author in authors:
             with self._onto:
                 # We escape XML character data
-                author.name = escape(author.name)
+                author.name = self._escape_value(author.name)
                 # We create the individual
                 author_object = self._onto.Author(author.name)
                 author_object.has_written.append(arxiv_document)
@@ -49,7 +63,7 @@ class OntologyService():
     def _add_primary_category(self: object, category, arxiv_document):
         """Add a primary category to the ontology"""
         with self._onto:
-            category = escape(category)
+            category = self._escape_value(category)
             category_object = self._onto.ArxivDocumentCategory(category)
             category_object.title.append(category)
             arxiv_document.has_as_primary_category.append(category_object)
@@ -58,7 +72,7 @@ class OntologyService():
         """Add categories to the ontology"""
         for category in categories:
             with self._onto:
-                category = escape(category)
+                category = self._escape_value(category)
                 category_object = self._onto.ArxivDocumentCategory(category)
                 category_object.title.append(category)
                 arxiv_document.has_as_category.append(category_object)
@@ -67,7 +81,7 @@ class OntologyService():
         """Add an arxiv document to the ontology"""
         with self._onto:
             if document.entry_id is not None:
-                entry_id = escape(document.entry_id)
+                entry_id = self._escape_value(document.entry_id)
             else:
                 # If entry_id is not None, we return because entry_id is mandatory
                 return None
@@ -76,21 +90,21 @@ class OntologyService():
             # Adding all data properties
             document_object.entry_id.append(entry_id)
             if document.updated is not None:
-                document_object.updated.append(escape(document.updated.strftime("%Y-%m-%dT%H:%M:%S")))
+                document_object.updated.append(self._escape_value(document.updated.strftime("%Y-%m-%dT%H:%M:%S")))
             if document.published is not None:
-                document_object.published.append(escape(document.published.strftime("%Y-%m-%dT%H:%M:%S")))
+                document_object.published.append(self._escape_value(document.published.strftime("%Y-%m-%dT%H:%M:%S")))
             if document.title is not None:
-                document_object.title.append(escape(document.title))
+                document_object.title.append(self._escape_value(document.title))
             if document.summary is not None:
-                document_object.summary.append(escape(document.summary))
+                document_object.summary.append(self._escape_value(document.summary))
             if document.comment is not None:
-                document_object.comment.append(escape(document.comment))
+                document_object.comment.append(self._escape_value(document.comment))
             if document.journal_ref is not None:
-                document_object.journal_ref.append(escape(document.journal_ref))
+                document_object.journal_ref.append(self._escape_value(document.journal_ref))
             if document.doi is not None:
-                document_object.doi.append(escape(document.doi))
+                document_object.doi.append(self._escape_value(document.doi))
             if document.pdf_url is not None:
-                document_object.pdf_url.append(escape(document.pdf_url))
+                document_object.pdf_url.append(self._escape_value(document.pdf_url))
             self._add_authors(document.authors, document_object)
             self._add_primary_category(document.primary_category, document_object)
             self._add_categories(document.categories, document_object)
@@ -101,7 +115,7 @@ class OntologyService():
         if named_entity.type == NamedEntityTypeEnum.PERSON:
             with self._onto:
                 # We escape XML character data
-                named_entity.text = escape(named_entity.text)
+                named_entity.text = self._escape_value(named_entity.text)
                 # We create the individual
                 person = self._foaf.Person(named_entity.text)
                 # We split the text after the first space
